a486fa42efb5548c6c6d200975661dc93b426c59
md21it181 <vatthipatidar@gmail.com>
2021-11-10 23:56:41 +0530

    Initial commit

--- app/src/main/java/com/myapp/idonor/Login.java
+++ app/src/main/java/com/myapp/idonor/Login.java
@@ -3,18 +3,35 @@ package com.myapp.idonor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.myapp.idonor.donor_form.DonorFormActivity;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login extends AppCompatActivity {
 
     EditText email, password;
     Button login, register;
+
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +43,12 @@ public class Login extends AppCompatActivity {
         login = findViewById(R.id.login);
         register = findViewById(R.id.register);
 
+
+
         @SuppressLint("WrongConstant") SharedPreferences sh = getSharedPreferences("db", MODE_APPEND);
         String user = sh.getString("email", "");
 
-        if(user.equals("admin")){
+        if(!(user.equals(""))){
             startActivity(new Intent(Login.this, MainActivity.class));
             finish();
         }
@@ -37,26 +56,120 @@ public class Login extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(email.getText().toString().equals("admin") && email.getText().toString().equals("admin")) {
+                progressDialog = new ProgressDialog(Login.this);
+                progressDialog.show();
+                progressDialog.setContentView(R.layout.progress_dialog);
+                progressDialog.getWindow().setBackgroundDrawableResource(
+                        android.R.color.transparent
+                );
+                progressDialog.setCancelable(false);
+                loginData();
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog = new ProgressDialog(Login.this);
+                progressDialog.show();
+                progressDialog.setContentView(R.layout.progress_dialog);
+                progressDialog.getWindow().setBackgroundDrawableResource(
+                        android.R.color.transparent
+                );
+                progressDialog.setCancelable(false);
+
+                registerData();
+            }
+        });
+    }
+    public void registerData(){
+        final RequestQueue requestQueue = Volley.newRequestQueue(Login.this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://smart-blood-4.000webhostapp.com/register.php", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(response.equals("inserted")) {
                     SharedPreferences sharedpreferences = getSharedPreferences("db", Context.MODE_PRIVATE);
                     SharedPreferences.Editor editor = sharedpreferences.edit();
                     editor.putString("email",email.getText().toString());
 
                     editor.commit();
 
+                    Toast.makeText(Login.this, "Registered", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(Login.this, MainActivity.class));
                     finish();
-
+                }else{
                 }
-            }
-        });
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                    startActivity(new Intent(Login.this, MainActivity.class));
-                finish();
+                progressDialog.cancel();
+
+
+                requestQueue.stop();
             }
-        });
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                requestQueue.stop();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+
+                params.put("email",email.getText().toString());
+                params.put("password",password.getText().toString());
+
+                return params;
+            }
+        };
+
+        requestQueue.add(stringRequest);
+
+    }
+    public void loginData(){
+        final RequestQueue requestQueue = Volley.newRequestQueue(Login.this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://smart-blood-4.000webhostapp.com/login.php", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(response.equals("incorrect")) {
+                    Toast.makeText(Login.this, "Incorrect information.", Toast.LENGTH_SHORT).show();
+                }else{
+                    SharedPreferences sharedpreferences = getSharedPreferences("db", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedpreferences.edit();
+                    editor.putString("email",email.getText().toString());
+
+                    editor.commit();
+
+                    Toast.makeText(Login.this, "Registered", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(Login.this, MainActivity.class));
+                    finish();
+                }
+
+                progressDialog.cancel();
+
+
+                requestQueue.stop();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                requestQueue.stop();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+
+                params.put("email",email.getText().toString());
+                params.put("password",password.getText().toString());
+
+                return params;
+            }
+        };
+
+        requestQueue.add(stringRequest);
+
     }
 }
--- app/src/main/java/com/myapp/idonor/MainActivity.java
+++ app/src/main/java/com/myapp/idonor/MainActivity.java
@@ -2,9 +2,12 @@ package com.myapp.idonor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.myapp.idonor.donor_form.DonorFormActivity;
 import com.myapp.idonor.donor_list.DonorListActivity;
@@ -13,12 +16,28 @@ import com.myapp.idonor.request_blood.RequestBloodActivity;
 public class MainActivity extends AppCompatActivity{
 
 
-
+    TextView logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logout = findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedpreferences = getSharedPreferences("db", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putString("email","");
+
+                editor.commit();
+
+                startActivity(new Intent(MainActivity.this, Login.class));
+                finish();
+            }
+        });
 
 
     }
